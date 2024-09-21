2081c7064f2d4dbb9a900663d46cf4962b25bc6c
aryachauhan <106148200+aryachauhan@users.noreply.github.com>
2024-09-21 19:23:23 +0530

    exploring get, post, delete api

--- src/app.js
+++ src/app.js
@@ -2,18 +2,22 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Arya", lastname: "Chauhan" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted successfully!..");
+});
+
 app.use("/test", (req, res) => {
   res.send("Hello from the test");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello from the hello");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello from the root");
-});
-
-app.listen(3000, () => {
+app.listen(7777, () => {
   console.log("Server running");
 });
