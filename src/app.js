10bcd8ffbcb8f002f8af1b82abb9b570f0d222c5
aryachauhan <106148200+aryachauhan@users.noreply.github.com>
2024-09-21 19:41:12 +0530

    explore different types of routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,37 @@ const express = require("express");
 
 const app = express();
 
+// work for ab and abc
+app.get("/ab?c", (req, res) => {
+  // b is optional
+  res.send({ firstName: "Arya", lastname: "Chauhan" });
+});
+
+// work for ad and abcd
+app.get("/a(bc)?d", (req, res) => {
+  // bc is optional
+  res.send({ firstName: "Arya", lastname: "Chauhan" });
+});
+
+// work for abc, abbc, abbbc, abb....bc
+app.get("/ab+c", (req, res) => {
+  // b can come any number of times
+  res.send({ firstName: "Arya", lastname: "Chauhan" });
+});
+
+// work for abcd, abAryacd
+app.get("/ab*cd", (req, res) => {
+  // anything can come in between b and c
+  res.send({ firstName: "Arya", lastname: "Chauhan" });
+});
+
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params); // return all request parans data , Ex: { userId: 101 };
+  console.log(req.query); // return all queryparams data, Ex: { name: "Arya" };
+
+  res.send({ firstName: "Arya", lastname: "Chauhan" });
+});
+
 app.get("/user", (req, res) => {
   res.send({ firstName: "Arya", lastname: "Chauhan" });
 });
@@ -19,5 +50,5 @@ app.use("/test", (req, res) => {
 });
 
 app.listen(7777, () => {
-  console.log("Server running");
+  console.log("Server is listening on port 7777...");
 });
